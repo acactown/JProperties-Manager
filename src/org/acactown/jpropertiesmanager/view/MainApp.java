cac015679fee1bf83b6a69cba7002cd7b7f52d25
Andrés Amado <acactown@gmail.com>
2011-10-15 19:31:08 -0500

    First commit for project JProperties Manager on GITHUB!

---
+++ src/org/acactown/jpropertiesmanager/view/MainApp.java
@@ -0,0 +1,43 @@
+package org.acactown.jpropertiesmanager.view;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ *
+ * @version 1.0
+ * @author acactown - acactown@gmail.com
+ */
+public class MainApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new MainView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of Main
+     */
+    public static MainApp getApplication() {
+        return Application.getInstance(MainApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(MainApp.class, args);
+    }
+}
